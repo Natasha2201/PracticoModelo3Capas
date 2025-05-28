a1c5410a3d5a4b907355fae401e9c344d3e07c28
Naomi <Naomi@DESKTOP-EL91EE6>
2025-05-27 20:48:39 -0500

    Agregar archivos de proyecto.

---
+++ CapaDeAccesoADatos/AccesoProducto.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace CapaDeAccesoADatos
+
+{
+
+    public class AccesoProducto
+
+    {
+        // Lista de productos (vacía al principio) 
+
+        private static List<string> productos = new List<string>();
+
+        // Obtener todos los productos 
+
+        public List<string> ObtenerTodosLosProductos()
+
+        {
+
+            return productos;
+
+        }
+
+        // Agregar un producto 
+
+        public void AgregarProducto(string producto)
+
+        {
+
+            productos.Add(producto);  // Agregar el producto a la lista 
+
+        }
+
+    }
+
+}
